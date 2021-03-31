6b01b23a7f9ecc290a767df03789c3c159945d84
IsabelAgAu <isabelagau@gmail.com>
2021-03-31 20:35:32 +0200

    Added Day 2 challenge: Check correct passwords

---
+++ Day1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode
+{
+    public class Day1Class
+    {
+        private const string inputDay1FilePath = @"..\..\..\inputDay1.txt";
+
+        public static void Day1Part1(int total)
+        {
+
+            var lines = File.ReadAllLines(inputDay1FilePath);
+            List<int> numbers = lines.Select(line => int.Parse(line))
+                                     .ToList();
+
+            // Which two numbers sum 2020
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == total)
+                    {
+                        Console.WriteLine($"The numbers {numbers[i]} and {numbers[j]} sum {total}");
+                        Console.WriteLine($"The key result is {numbers[i] * numbers[j]}");
+                    }
+                }
+            }
+
+        }
+
+        public static void Day1Part2(int total)
+        {
+
+            var lines = File.ReadAllLines(inputDay1FilePath);
+            List<int> numbers = lines.Select(line => int.Parse(line))
+                                     .ToList();
+
+            // Which three numbers sum 2020
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    for (int k = j + 1; k < numbers.Count; k++)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == total)
+                        {
+                            Console.WriteLine($"The numbers {numbers[i]}, {numbers[j]} and {numbers[k]} sum {total}");
+                            Console.WriteLine($"The key result is {numbers[i] * numbers[j] * numbers[k]}");
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public static void ExecuteDay1(int total)
+        {
+            var numbers = FindNumbersThatSumValue(total);
+            numbers.ForEach(number => Console.WriteLine($"The numbers {number} and {GetComplementary(number, total)} sum {total}"));
+        }
+        private static List<int> FindNumbersThatSumValue(int sumValue)
+        {
+            //Function Names
+            //Constants
+            //String interpolation
+            //LINQ
+
+            var lines = File.ReadAllLines(inputDay1FilePath);
+            List<int> numbers = lines.Select(line => int.Parse(line))
+                                     .ToList();
+
+            var numbersWithComplementary = numbers.Where(ComplementaryExist(sumValue, numbers))
+                                                  .ToList();
+
+            RemoveComplementaries(sumValue, numbersWithComplementary);
+            return numbersWithComplementary;
+        }
+
+        private static void RemoveComplementaries(int sumValue, List<int> numbersWithComplementary)
+        {
+            for (var i = 0; i < numbersWithComplementary.Count(); i++)
+            {
+                var number = numbersWithComplementary[i];
+                var complementary = GetComplementary(number, sumValue);
+                numbersWithComplementary.Remove(complementary);
+            }
+        }
+
+        private static Func<int, bool> ComplementaryExist(int sumValue, List<int> numbers)
+        {
+            return number => numbers.Exists(listNumber => listNumber == sumValue - number);
+        }
+
+        public static int GetComplementary(int number, int total)
+        {
+            return total - number;
+        }
+
+
+    }
+}
---
+++ Day2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventCode
+{
+    class Day2Class
+    {
+
+        private const string inputDay2FilePath = @"..\..\..\inputDay2.txt";
+        private const string blankSpliter = " ";
+        private const string twoDotSpliter = ":";
+        private const string spaceRegex = @"\s";
+
+        public static void Day2Part1()
+        {
+            var lines = File.ReadAllLines(inputDay2FilePath);
+            List<string> passwordList = lines.Select(line => line).ToList();
+
+
+            int validPasswords = passwordList.Count(IsValidPassword);
+
+            /*
+            for (int i = 0; i< passwordList.Count; i++) {
+                auxSplit = passwordList[i].Split(":");
+                password = auxSplit[1];
+                auxSplit = auxSplit[0].Split(" ");
+                letter = auxSplit[1];
+                rule = auxSplit[0];
+
+                password = Regex.Replace(password, @"\s+", "");
+                if (IsPasswordValidPart2(letter, password, rule))
+                {
+                    //Console.WriteLine($"Valid password: {password} with rule {rule} and letter {letter}");
+                    validPasswords++;
+                }
+            }*/
+
+            Console.WriteLine($"There are {validPasswords} valid passwords");
+        }
+
+        private static bool IsValidPassword(string line)
+        {
+                string rule;
+                string letter;
+                string password;
+                string[] auxSplit;
+
+                auxSplit = line.Split(twoDotSpliter);
+                password = auxSplit[1];
+                auxSplit = auxSplit[0].Split(blankSpliter);
+                letter = auxSplit[1];
+                rule = auxSplit[0];
+
+                password = Regex.Replace(password, spaceRegex, string.Empty);
+
+                //return IsPasswordValid(letter, password, rule);
+                return CheckPasswordPart2(letter, password, rule);
+            
+        }
+
+        private static bool CheckPassword(string letter, string password, string interval)
+        {
+            int count = password.Count(x => x.ToString().Equals(letter));
+
+            string[] limit = interval.Split("-");
+
+            if (limit.Count() != 2) return false;
+
+            if (count >= int.Parse(limit[0]) && count <= int.Parse(limit[1]))
+                return true;
+
+            return false;
+        }
+
+        private static bool CheckPasswordPart2(string letter, string password, string interval) 
+        {
+            string[] limit = interval.Split("-");
+            
+            if (
+                (password[int.Parse(limit[0]) - 1].Equals(char.Parse(letter)) 
+                && !password[int.Parse(limit[1]) - 1].Equals(char.Parse(letter))) ||
+                (!password[int.Parse(limit[0]) - 1].Equals(char.Parse(letter)) 
+                && password[int.Parse(limit[1]) - 1].Equals(char.Parse(letter))))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
